ac07beb6ca455ca0f067982d4390722a4cfa7025
rajankumary2k <rajankumary2k@gmail.com>
2023-10-13 13:45:48 +0530

    New folder added

---
+++ Methods/methods.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type rectangle struct {
+	width, height int
+}
+
+func (r *rectangle) area() int {
+	return r.width * r.height
+}
+func (r *rectangle) perimeter() int {
+	return 2*r.width + 2*r.height
+}
+
+func main() {
+	r := rectangle{width: 10, height: 5}
+	fmt.Println("area", r.area())
+	fmt.Println("perimeter", r.perimeter())
+}
